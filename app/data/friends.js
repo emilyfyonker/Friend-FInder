5ee5d7d3556fccaf7b98a98193f706ca27660245
Emily Yonker <emilyfyonker@protonmail.com>
2019-05-06 19:25:44 -0400

    1st push

---
+++ app/data/friends.js
@@ -0,0 +1,12 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of submitted surverys.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var surveysArray = [
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = surveysArray;
